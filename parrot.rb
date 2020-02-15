e991a18a4e3d5ea68c14c2b40bc07560ff0a5935
deedee medina <deedee.medinacorp@gmail.com>
2020-02-15 22:21:00 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -3,12 +3,7 @@ def parrot (phrase = "Squawk!")
   puts (phrase)
   parrot
 end
-phrase
-def parrot (phrase = "Squawk!")
-  chirp = phrase
-  puts (phrase)
-  chirp
-end
+
 
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
